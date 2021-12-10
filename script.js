45249287437731578bc167a48b3b39be00da02ad
André Rodrigues <andre360.ars@gmail.com>
2021-12-10 16:12:51 -0300

    Requisito 5 - cria função para adicionar tarefas a lista

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let button = document.getElementById('criar-tarefa')
+let list  = document.getElementById("lista-tarefas")
+let input = document.getElementById("texto-tarefa")
 
 function addOn() {
-    
+   let listItem = document.createElement('li');
+
+   listItem.innerText = input.value;
+   list.appendChild(listItem);
+   input.value = ''
 }

902aae058cc3dbbfd41c005f7330d2d72a28fe8c
André Rodrigues <andre360.ars@gmail.com>
2021-12-18 21:11:07 -0300

    Requisito 7 - cria função, para trocar cor de fundo, ao clicar em uma tarefa

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 let button = document.getElementById('criar-tarefa')
 let list  = document.getElementById("lista-tarefas")
 let input = document.getElementById("texto-tarefa")
+let tasks = document.getElementsByClassName('tasks')
+
 
 function addOn() {
    let listItem = document.createElement('li');
 
    listItem.innerText = input.value;
+   listItem.className = 'tasks'
    list.appendChild(listItem);
    input.value = ''
 }
 
 button.addEventListener('click', addOn)
+
+
+function paintTask(event) {
+   for (let index = 0; index < tasks.length; index += 1) {
+      document.querySelectorAll('#lista-tarefas li')[index].className = 'tasks';
+   } 
+
+   event.target.className = 'selected tasks'
+}
+
+document.getElementById("lista-tarefas").addEventListener('click', paintTask);
